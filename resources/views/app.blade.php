4a724dd762ca89e3ee19d2435f043f450ec1a90b
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-11-04 17:52:15 +0700

    testing new script with new endpoint

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -47,7 +47,7 @@
       const STORE_URL =
         (window.TRAFFIC_ENDPOINT) ||
         (document.currentScript?.dataset?.endpoint) ||
-        (location.origin + ':3000/api/storeTrafficData');
+        (location.origin + '/api/storeTrafficData');
       // ============================================
 
       // Track last sent URL and what to use as "ref" next time
